7b1b7eacc019cd2fac532c439f0b61dcb2850417
Kolesin Andrei <kolesinandrei1910@gmail.com>
2018-08-28 21:55:39 +0300

    пофикшен парсер сайта

--- parser.py
+++ parser.py
@@ -7,11 +7,12 @@ import re
 def parse(html):
     a = html.find('бакалавров')
     if a != -1:
-        q = html.find('href', a)
-        link_begin = html.find('"', q) + 1
-        link_end = html.find('"', link_begin)
-        date_begin = html.find('(', link_end)
+        date_begin = html.find('(', a)
         date_end = html.find(')', date_begin) + 1
+        href = html.find('href', a)
+        link_begin = html.find('"', href) + 1
+        link_end = html.find('"', link_begin)
+
         link = html[link_begin:link_end]
         date = html[date_begin:date_end]
         reg = re.compile('[а-яА-Я]')
